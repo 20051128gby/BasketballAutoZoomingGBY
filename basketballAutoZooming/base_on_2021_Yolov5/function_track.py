ade12f34c425ff1847b974c5c7a090c39d75113e
gby <zhenwuyugby@163.com>
2024-03-28 15:05:31 +0800

    提取图片数据集

--- basketballAutoZooming/base_on_2021_Yolov5/function_track.py
+++ basketballAutoZooming/base_on_2021_Yolov5/function_track.py
@@ -1,17 +1,48 @@
 import json
 import cv2
+import numpy as np
 from utils.plots import plot_one_box
-import matplotlib.pyplot as plt
-from IPython.display import display, clear_output
+# import matplotlib.pyplot as plt
+# from IPython.display import display, clear_output
 from detector import Get_Pred
+from PIL import Image
+from keras.models import load_model  # TensorFlow is required for Keras to work
+from PIL import Image, ImageOps  # Install pillow instead of PIL
+import numpy as np
+final_list=[]
 capture = cv2.VideoCapture(r'E:\vscodeP\data\Video\3月25日.mp4')
 ret, img = capture.read()
 frame_height, frame_width, channels = img.shape
             # 定义视频文件的名称和编码格式
-video_filename = 'output.avi'
-fourcc = cv2.VideoWriter_fourcc(*'MJPG')
+video_filename = 'output.mp4'
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
 fps = 20
 video = cv2.VideoWriter(video_filename, fourcc, fps, (frame_width, frame_height))
+
+
+def resize_with_white_background(og_list,target_size=(224, 224), background_color=(255, 255, 255)):
+    crop_list=[]
+    for image in og_list:
+
+    # Resize the image while maintaining aspect ratio
+        
+        image = Image.fromarray(image, 'RGB')
+        image.thumbnail(target_size, Image.LANCZOS)
+        
+
+    # Create a new image with white background
+        new_image = Image.new("RGB", target_size, background_color)
+
+    # Calculate position to paste the resized image
+        left = (target_size[0] - image.size[0]) // 2
+        top = (target_size[1] - image.size[1]) // 2
+
+    # Paste the resized image onto the white background
+        new_image.paste(image, (left, top))
+
+    # Save the new image
+        crop_list.append(new_image)
+    return crop_list
 
 
 def crop(image, boxes_list):
@@ -30,8 +61,12 @@ def crop(image, boxes_list):
 
         # 将裁剪后的图像添加到列表中
         cropped_images.append(cropped_image)
-        cv2.imwrite(str(i)+".jpg",cropped_image)
-        i+=1
+        
+        # square_image = np.array(pad_to_square(cropped_image))
+        # cv2.imwrite(str(i)+"SquareWhite.jpg",square_image)
+        # cv2.imwrite(str(i)+".jpg",cropped_image)
+        # print(1)
+        # i+=1
 
     return cropped_images
 
@@ -41,47 +76,50 @@ def calRatio(x):
     return (x2-x1)/(y2-y1)
 
 
-def display_cropped_images(cropped_images):
+# def display_cropped_images(cropped_images):
    
-    # 定义每行显示的图像数量
-    IMAGES_PER_ROW = 4
+#     # 定义每行显示的图像数量
+#     IMAGES_PER_ROW = 4
 
-    num_rows = (len(cropped_images) + IMAGES_PER_ROW - 1) // IMAGES_PER_ROW
-    fig, axes = plt.subplots(num_rows, IMAGES_PER_ROW, figsize=(IMAGES_PER_ROW * 5, num_rows * 5))
-    for i, img in enumerate(cropped_images):
-        # 计算当前图像的行和列
-        row = i // IMAGES_PER_ROW
-        col = i % IMAGES_PER_ROW
+#     num_rows = (len(cropped_images) + IMAGES_PER_ROW - 1) // IMAGES_PER_ROW
+#     fig, axes = plt.subplots(num_rows, IMAGES_PER_ROW, figsize=(IMAGES_PER_ROW * 5, num_rows * 5))
+#     for i, img in enumerate(cropped_images):
+#         # 计算当前图像的行和列
+#         row = i // IMAGES_PER_ROW
+#         col = i % IMAGES_PER_ROW
 
-        axes[row, col].imshow(img)
-        axes[row, col].axis('off')  
+#         axes[row, col].imshow(img)
+#         axes[row, col].axis('off')  
 
-    plt.subplots_adjust(wspace=0.1, hspace=0.1)
+#     plt.subplots_adjust(wspace=0.1, hspace=0.1)
 
-    # 显示整个图像网格
-    plt.show()
+#     # 显示整个图像网格
+#     plt.show()
 
-    # 创建翻页功能
-    def next_page():
-        clear_output(wait=True)
-        display(fig)
-    def prev_page():
-        clear_output(wait=True)
-        display(fig)
-    plt.gcf().canvas.mpl_connect('key_press_event', lambda event: on_key(event, next_page, prev_page))
+#     # 创建翻页功能
+#     def next_page():
+#         clear_output(wait=True)
+#         display(fig)
+#     def prev_page():
+#         clear_output(wait=True)
+#         display(fig)
+#     plt.gcf().canvas.mpl_connect('key_press_event', lambda event: on_key(event, next_page, prev_page))
 
-def on_key(event, next_func, prev_func):
-    if event.key == 'right':
-        next_func()  # 右箭头键翻到下一页
-    elif event.key == 'left':
-        prev_func()  # 左箭头键翻到上一页
+# def on_key(event, next_func, prev_func):
+#     if event.key == 'right':
+#         next_func()  # 右箭头键翻到下一页
+#     elif event.key == 'left':
+#         prev_func()  # 左箭头键翻到上一页
+
+
+
 
 
 
 def detect_video(path):
     capture = cv2.VideoCapture(path)
     temp = 0
-    while True:
+    while temp<1:
         ret, img = capture.read()
         temp+=1
         if ret is not True:
@@ -91,15 +129,26 @@ def detect_video(path):
         value = json.loads(value_json)
         pred_xyxy = value['xyxy']
         cropList=crop(img,pred_xyxy)
+
+        #展示裁剪后的人类图片
         #display_cropped_images(cropList)
+
+
         for xyxy in pred_xyxy:
-            plot_one_box(xyxy, img, label=None, color=(255, 255, 0), line_thickness=1)
+            #plot_one_box(xyxy, img, label=None, color=(255, 255, 0), line_thickness=1)
             print((xyxy))
-        cv2.imwrite("whole.jpg", img)  
-        a = int(input("执行下一帧"))
-        cv2.imshow('Video', img)      
+        
+
+        # cv2.imwrite("whole.jpg", img)  
+        
+        global final_list 
+        final_list= resize_with_white_background(cropList)
+        #final_list[1].save("output_image.png", "PNG")
+        #cv2.imshow("final",np.array(final_list[1]))
+        #a = int(input("执行下一帧"))
+        # cv2.imshow('Video', img)      
              
- 
+
         #video.write(img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             
@@ -113,4 +162,52 @@ if __name__ == '__main__':
     pred_path = r'E:\vscodeP\data\Video\3月25日.mp4'
     detect_video(pred_path)
     video.release()
+# Load the model
+    model = load_model(r"E:\vscodeP\AI\basketballAutoZooming\base_on_2021_Yolov5\keras_model.h5", compile=False)
+
+# Load the labels
+    class_names = open(r"E:\vscodeP\AI\basketballAutoZooming\base_on_2021_Yolov5\labels.txt", "r").readlines()
+    size = (224, 224)
+
+
+    i = 0
+    for image in final_list:
+        cv2.imwrite(str(i)+".jpg",np.array(image))  
+    # Disable scientific notation for clarity
+        i+=1
+        np.set_printoptions(suppress=True)
+
+
+# Create the array of the right shape to feed into the keras model
+# The 'length' or number of images you can put into the array is
+# determined by the first position in the shape tuple, in this case 1
+        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+# Replace this with the path to your image
+        
+
+# resizing the image to be at least 224x224 and then cropping from the center
+        
+        image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+
+# turn the image into a numpy array
+        image_array = np.asarray(image)
+
+# Normalize the image
+        normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+
+# Load the image into the array
+        data[0] = normalized_image_array
+
+# Predicts the model
+        prediction = model.predict(data)
+        index = np.argmax(prediction)
+        class_name = class_names[index]
+        confidence_score = prediction[0][index]
+
+# Print prediction and confidence score
+        print("Class:", class_name[2:], end="")
+        print("Confidence Score:", confidence_score)
+
+
     cv2.destroyAllWindows()
